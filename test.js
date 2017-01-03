d33f6d335489bb683267f65ad175801cb1090be4
Gary Katsevman <git@gkatsev.com>
2017-01-03 16:04:48 -0500

    better test

--- test.js
+++ test.js
@@ -1,13 +1,22 @@
 const sh = require('shelljs');
 const path = require('path');
 
-const command = `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE}`;
+// by default we want to run against the current commit
+let commits = process.env.TRAVIS_COMMIT;
+const commitRange = process.env.TRAVIS_COMMIT_RANGE;
 
-console.log(command);
+// if we're not on master or TRAVIS_COMMIT_RANGE isn't empty, use the range
+if (process.env.TRAVIS_BRANCH !== 'master' && commitRange) {
+  commits = commitRange;
+}
+
+const command = `git diff --name-only ${commits}`;
+
+console.log('Running the following command: ', command);
 
 const output = sh.exec(command, {async: false, silent: true}).stdout;
 
-const files = output.split('\n');
+const files = output.split('\n').filter(Boolean);
 
 if (files.every((file) => file.startsWith('docs') || path.extname(file) === '.md')) {
   console.log('!!!!');
