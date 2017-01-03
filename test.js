03bc200f93c3d17d53f8724f98e4dd22f1b08cdd
Gary Katsevman <git@gkatsev.com>
2017-01-03 15:52:24 -0500

    docs

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const sh = require('shelljs');
+const path = require('path');
 
 const command = `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE}`;
 
 console.log(command);
 
-const output = sh.exec(command, {async: false}).stdout;
+const output = sh.exec(command, {async: false, silent: true}).stdout;
 
-console.log('output', output);
+const files = output.split('\n');
+
+if (files.every((file) => file.startsWith('docs') || path.extname(file) === '.md')) {
+  console.log('!!!!');
+} else {
+  console.log('????');
+}
+
+//console.log('output', output);
