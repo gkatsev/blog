41ba6912719bb8f96738edcd48758b33d7f02609
Gary Katsevman <git@gkatsev.com>
2017-01-03 16:45:52 -0500

    better commands

--- test.js
+++ test.js
@@ -4,10 +4,18 @@ const path = require('path');
 // by default we want to run against the current commit
 let command = `git diff-tree --no-commit-id --name-only -r ${process.env.TRAVIS_COMMIT}`
 
+// if we have a range, compare it against master
+if (process.env.TRAVIS_COMMIT_RANGE) {
+  command = `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE} master`;
+}
+
+const pr = JSON.parse(process.env.TRAVIS_PULL_REQUEST);
+
 console.log(process.env.TRAVIS_BRANCH, process.env.TRAVIS_PULL_REQUEST, process.env.TRAVIS_PULL_REQUEST_BRANCH);
-// if we're not on master
-if (process.env.TRAVIS_BRANCH !== 'master' && process.env.TRAVIS_PULL_REQUEST_BRANCH) {
-  command = `git diff --name-only master`;
+
+// if we're in a PR, compare the PR against master
+if (pr) {
+  command = `git diff --name-only ${process.env.TRAVIS_PULL_REQUEST_BRANCH} master`;
 }
 
 console.log('Running the following command: ', command);
