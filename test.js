7fc2e96c7cd65380f1193ce072bc320ecd3a3811
Gary Katsevman <git@gkatsev.com>
2017-01-03 16:59:38 -0500

    run all commands

--- test.js
+++ test.js
@@ -2,33 +2,40 @@ const sh = require('shelljs');
 const path = require('path');
 
 // by default we want to run against the current commit
-let command = `git diff-tree --no-commit-id --name-only -r ${process.env.TRAVIS_COMMIT}`
+const SINGLE_COMMIT = `git diff-tree --no-commit-id --name-only -r ${process.env.TRAVIS_COMMIT}`
+const COMMIT_RANGE = `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE} master`;
+const PR_DIFF = `git diff --name-only ${process.env.TRAVIS_PULL_REQUEST_BRANCH} master`;
+
+console.log(process.env.TRAVIS_BRANCH, process.env.TRAVIS_PULL_REQUEST, process.env.TRAVIS_PULL_REQUEST_BRANCH, process.env.TRAVIS_COMMIT, process.env.TRAVIS_COMMIT_RANGE);
 
 // if we have a range, compare it against master
-if (process.env.TRAVIS_COMMIT_RANGE) {
-  command = `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE} master`;
-}
+//if (process.env.TRAVIS_COMMIT_RANGE) {
+//}
 
-const pr = JSON.parse(process.env.TRAVIS_PULL_REQUEST);
+//const pr = JSON.parse(process.env.TRAVIS_PULL_REQUEST);
 
-console.log(process.env.TRAVIS_BRANCH, process.env.TRAVIS_PULL_REQUEST, process.env.TRAVIS_PULL_REQUEST_BRANCH);
 
-// if we're in a PR, compare the PR against master
-if (pr) {
-  command = `git diff --name-only ${process.env.TRAVIS_PULL_REQUEST_BRANCH} master`;
-}
+//// if we're in a PR, compare the PR against master
+//if (pr) {
+//}
 
-console.log('Running the following command: ', command);
+//console.log('Running the following command: ', command);
 
-const output = sh.exec(command, {async: false, silent: true}).stdout;
+const singleout = sh.exec(SINGLE_COMMIT, {async: false, silent: true}).stdout;
+const rangeout = sh.exec(COMMIT_RANGE, {async: false, silent: true}).stdout;
+const prout = sh.exec(PR_DIFF, {async: false, silent: true}).stdout;
 
-const files = output.split('\n').filter(Boolean);
+console.log(SINGLE_COMMIT, '\n', singleout);
+console.log(COMMIT_RANGE, '\n', rangeout);
+console.log(PR_DIFF, '\n', prout);
 
-console.log(output, files);
-if (files.every((file) => file.startsWith('docs') || path.extname(file) === '.md')) {
-  console.log('!!!!');
-} else {
-  console.log('????');
-}
+//const files = output.split('\n').filter(Boolean);
+
+//console.log(output, files);
+//if (files.every((file) => file.startsWith('docs') || path.extname(file) === '.md')) {
+  //console.log('!!!!');
+//} else {
+  //console.log('????');
+//}
 
 //console.log('output', output);
