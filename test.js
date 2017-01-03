2e9c4dfc1331dd7e019bbd4285588d483907cca3
Gary Katsevman <git@gkatsev.com>
2017-01-03 15:48:45 -0500

    better log

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 const sh = require('shelljs');
 
-sh.exec(`git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE}`);
+const command = `git diff --name-only ${process.env.TRAVIS_COMMIT_RANGE}`;
+
+console.log(command);
+
+const output = sh.exec(command, {async: false}).stdout;
+
+console.log('output', output);
